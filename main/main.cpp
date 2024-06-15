b084248a822ea4a4bb4f3e8b8bcd8cfbf7b9036b
ronny-antoon <90724911+ronny-antoon@users.noreply.github.com>
2024-06-15 16:57:56 +0300

    working patch

--- main/main.cpp
+++ main/main.cpp
@@ -1,11 +1,10 @@
 #include <esp_matter.h>
 #include <nvs_flash.h>
 
-#include <ButtonDevice.hpp>
-#include <FanDevice.hpp>
+#include <ButtonModule.hpp>
+#include <LightAccessory.hpp>
 #include <LightDevice.hpp>
-#include <PlugInDevice.hpp>
-#include <WindowDevice.hpp>
+#include <RelayModule.hpp>
 
 #include "app_callback.hpp"
 #include "metric_tracker.hpp"
@@ -24,26 +23,16 @@ extern "C" void app_main() {
       node, &aggregator_config, esp_matter::endpoint_flags::ENDPOINT_FLAG_NONE, nullptr);
 
   /* Create the LightDevice */
-  LightDevice *lightDevice = new LightDevice("LightDevice", GPIO_NUM_23, GPIO_NUM_34, aggregator);
-
-  /* Create the FanDevice */
-  FanDevice *fanDevice = new FanDevice("FanDevice", GPIO_NUM_22, GPIO_NUM_35, aggregator);
-
-  /* Create the PlugInDevice */
-  PlugInDevice *plugInDevice = new PlugInDevice("PlugInDevice", GPIO_NUM_21, GPIO_NUM_32, aggregator);
-
-  /* Create the ButtonDevice */
-  ButtonDevice *buttonDevice = new ButtonDevice("ButtonDevice", GPIO_NUM_18, aggregator);
-
-  /* Create the WindowDevice */
-  WindowDevice *windowDevice =
-      new WindowDevice("WindowDevice", GPIO_NUM_2, GPIO_NUM_17, GPIO_NUM_5, GPIO_NUM_26, 10, 10, aggregator);
+  ButtonModule *buttonModule = new ButtonModule(GPIO_NUM_5);
+  RelayModule *relayModule = new RelayModule(GPIO_NUM_2);
+  LightAccessory *lightAccessory = new LightAccessory(relayModule, buttonModule);
+  LightDevice *lightDevice = new LightDevice("LightDevice", lightAccessory, aggregator);
 
   esp_matter::start(app_event_cb);
 
-  /* Initialize the metric tracker */
-  metric_tracker_init("http://192.168.3.196:3001/metrics", "DevKitModule", 800);
+  // /* Initialize the metric tracker */
+  // metric_tracker_init("http://192.168.3.196:3001/metrics", "DevKitModule", 800);
 
-  /* Send the metrics to the server automatically every 30 seconds */
-  metric_tracker_auto_send(30, true, true);
+  // /* Send the metrics to the server automatically every 30 seconds */
+  // metric_tracker_auto_send(30, true, true);
 }
